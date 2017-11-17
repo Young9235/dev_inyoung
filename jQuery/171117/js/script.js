d38516f72527d5b055cbc1726bb1eb43ec73b64a
inyoung <inyoung9235@daum.net>
2017-11-17 17:23:32 +0900

    jQuery

---
+++ jQuery/171117/js/script.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function(){           // 문서가 읽힐 때까지 기다려라(있어야 실행된다)
+    $('h2').css("background-color","pink");
+    });
+// 바디 맨 아래에 제이쿼리 코드를 넣어도 된다.
+// html을 실행후 제이쿼리를 돌려라 라는 의미
+
+*/
+
+$(document).ready(function(){
+    $('h1').css("background","pink");
+    $('p').css("background","blue");
+});
